ca185edcf8dd77b52944f47ae241b5f55a88870b
Bryan Dollery <bryan.dollery@gmail.com>
2020-08-13 12:18:55 +0300

    openning ssh port

--- lab/main.tf
+++ lab/main.tf
@@ -130,7 +130,7 @@ resource "aws_security_group" "webserver" {
     from_port       = 22
     to_port         = 22
     protocol        = "tcp"
-    security_groups = [aws_security_group.bastion.id]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
